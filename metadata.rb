e6d64719d92ae08c2922787dd1592276e51c7a17
Rajat Vig <rajat.vig@gmail.com>
2013-09-05 09:14:28 -0700

    ITSCMSPL-000 [Rajat, Mrak] Revert Removing dependency to APT
    This reverts commit e48fa259a2585286d731190f66d6b15df0a14569.

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,7 @@ recipe "mongodb::configserver", "Installs and configures a configserver for mong
 recipe "mongodb::shard", "Installs and configures a single shard"
 recipe "mongodb::replicaset", "Installs and configures a mongodb replicaset"
 
-# depends "apt", ">= 1.8.2"
+depends "apt", ">= 1.8.2"
 depends "yum"
 
 %w{ ubuntu debian freebsd centos redhat fedora amazon scientific}.each do |os|
@@ -64,11 +64,11 @@ attribute "mongodb/replicaset_name",
 attribute "mongodb/enable_rest",
   :display_name => "Enable Rest",
   :description => "Enable the ReST interface of the webserver"
-
+  
 attribute "mongodb/smallfiles",
   :display_name => "Use small files",
   :description => "Modify MongoDB to use a smaller default data file size"
-
+  
 attribute "mongodb/bind_ip",
   :display_name => "Bind address",
   :description => "MongoDB instance bind address",
